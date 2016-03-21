769f3433a76ee1b6c85db9acc3016b906b90230b
Jake Kirkbride <jkirkbride@rjtechnologies.com>
2016-03-21 10:35:39 -0400

    Added Dependencies via Bower

--- ASH/ASH/www/scripts/index.js
+++ ASH/ASH/www/scripts/index.js
@@ -12,6 +12,8 @@
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
+        // Handle SPA events
+        //document.addEventListener( '', )
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var element = document.getElementById("deviceready");
         element.innerHTML = 'Device Ready';
